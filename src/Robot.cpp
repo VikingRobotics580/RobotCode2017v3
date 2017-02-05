241bc5cce3998455ccddbe88d85a00096e1bf55b
SCHOOLASUS\Logan <dreschl@campbellhall.org>
2017-02-05 15:32:33 -0800

    Arduino Code integrated, make file and batch file made to push code to robot w/ cmd prompt, and QOL Changes

--- src/Robot.cpp
+++ src/Robot.cpp
@@ -1,26 +1,29 @@
 #include <WPILib.h>
 
-/*
- * The Robot Load Order, after calling START_ROBOT_CLASS, is as follows:
- *
- * Constructor -> RobotInit -> DisabledInit -> DisabledPeriodic
- * Once a mode changes, the function execution order looks like the following:
- * PreviousModePeriodic -> NextModeInit -> NextModePeriodic
- *
- */
+/*  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *
+ * The Robot Load Order, after calling START_ROBOT_CLASS, is as follows:       *
+ *                                                                             *
+ * Constructor -> RobotInit -> DisabledInit -> DisabledPeriodic                *
+ * Once a mode changes, the function execution order looks like the following: *
+ * PreviousModePeriodic -> NextModeInit -> NextModePeriodic                    *
+ *                                                                             *
+ *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  */
 
 class Robot : public IterativeRobot {
-    public:        Robot():
-            IterativeRobot(),
-            frontLeft(0),
-            backLeft(1),
-            frontRight(2),
-            backRight(3),
-            shooter(4)
+    public:
+	Robot():
+            IterativeRobot(),//constructor
+            //These constructions are given the number of the PWM port on the roboRio
+            frontLeft(0),  //port 0
+            backLeft(1),   //port 1
+            frontRight(2), //port 2
+            backRight(3),  //port 3
+            shooter(4)     //port 4
         {
             // You should only initialize value here. Try not to have any other
             //  executable code here.
-            joy = new Joystick(0);
+            joy = new Joystick(0);  //Drive Joystick
+            joy2 = new Joystick(1); //Arduino "Joystick"
             drive = NULL;
         }
 
@@ -43,8 +46,8 @@ class Robot : public IterativeRobot {
             // It is important to note that all code here will run as though
             //  autonomous mode is endless, so you have to carefully time how
             //  long your code will run for.
-            // Just drive straight.
-            drive->MecanumDrive_Cartesian(0.0f, 0.0f, 0.0f, 0.0f);
+            drive->MecanumDrive_Cartesian(0.5f, 0.0f, 0.0f, 0.0f);
+            // The Current code is to: Just drive straight.
         }
 
         void TeleopInit() {
@@ -62,11 +65,11 @@ class Robot : public IterativeRobot {
                                           joy->GetTwist(), 0.0f);
 
 
-            if(joy->GetRawButton(1)){
-            	shooter.Set(joy->GetThrottle()); /*GET VALUE FROM SHOOTER BUILD TEAM*/
-
+            if(joy2->GetRawButton(1)){
+            	shooter.Set((((joy2->GetX())+1)/4)+0.5); //This puts power between 50% and 100%
+            	/*IMPORTANT NOTE: THIS HAS BEEN SET UP FOR ARDUINO*/
             } else {
-            	shooter.Set(0.0f);
+            	shooter.Set(0.0f); //Aka: do nothing
             }
 
 
@@ -90,13 +93,14 @@ class Robot : public IterativeRobot {
         //  run during the competitions.
 
     private:
-        Joystick* joy;
-        Talon frontLeft;
-        Talon backLeft;
-        Talon frontRight;
-        Talon backRight;
-        Talon shooter;
-        RobotDrive* drive;
+        Joystick* joy; //Drive Joystick
+        Joystick* joy2; //Arduino "Joystick"
+        Talon frontLeft; //Front-left Mecanum wheel
+        Talon backLeft; //Back-left Mecanum wheel
+        Talon frontRight; //Front-right Mecanum wheel
+        Talon backRight; //Back-right Mecanum wheel
+        Talon shooter; //Shooter Motor
+        RobotDrive* drive; //Drive Train
 };
 
 START_ROBOT_CLASS(Robot);
